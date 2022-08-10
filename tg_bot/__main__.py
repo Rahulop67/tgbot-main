4050fadf1d624ce685d0ecbef0aabb9cd04eee52
Rahulop67 <110233073+Rahulop67@users.noreply.github.com>
2022-08-10 20:03:48 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -18,7 +18,7 @@ from tg_bot.modules.helper_funcs.chat_status import is_user_admin
 from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
-*Hi {} Iam {}, 
+*Hi {}, Iam {}*. 
 *An Advanced Group Manager Bot to Manage your group's effectively.*
 *To know about my commands to and their uses send /help..*
 """
