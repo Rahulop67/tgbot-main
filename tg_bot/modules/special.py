9639b12abe360370c151f192f3f40381118f4ff8
Rahulop67 <110233073+Rahulop67@users.noreply.github.com>
2022-08-12 12:56:25 +0530

    Update special.py

--- tg_bot/modules/special.py
+++ tg_bot/modules/special.py
@@ -2,24 +2,25 @@ from tg_bot import dispatcher
 
 
 __help__ = """
-Owner only:*
-- /getlink *chatid*: Get the invite link for a specific chat.
+*Owner only:*
+- /getlink chatid: Get the invite link for a specific chat.
 - /banall: Ban all members from a chat
-- /leavechat *chatid : leave a chat
+- /leavechat chatid : leave a chat
 - /logchannel: get log channel info
 - /setlog: set the log channel.
 - /unsetlog: unset the log channel.
-- /quickscope *chatid* *userid*: Ban user from chat.
-- /quickunban *chatid* *userid*: Unban user from chat.
-- /snipe *chatid* *string*: Make me send a message to a specific chat.
-- /rban *chatid* *userid" remotely ban a user from a chat
-- /runban *chatid" *userid* remotely unban a user from a chat
+- /quickscope chatid userid: Ban user from chat.
+- /quickunban chatid userid: Unban user from chat.
+- /snipe chatid string: Make me send a message to a specific chat.
+- /rban chatid userid remotely ban a user from a chat
+- /runban chatid" userid remotely unban a user from a chat
 - /Stats: check bot's stats
 - /chatlist: get chatlist 
 - /gbanlist: get gbanned users list
 - /gmutelist: get gmuted users list
 - /Gban : Global ban a user
 - /Ungban : Ungban a user
+- /gbanstat <on/off/yes/no>: Will disable the effect of global bans on your group,
 - /Gmute : Gmute a user
 - /Ungmute : Ungmute a user
 - /listsudo Gives a list of sudo users
